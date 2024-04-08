cad67bb12f5f0611de62dec80b66fa3efb3210d1
IcSitis <161718781+IcSitis@users.noreply.github.com>
2024-04-08 20:48:47 +0300

    Add files via upload

---
+++ lab 3/lessons.py
@@ -0,0 +1,41 @@
+import links as l
+lessonsList = ["Урок 1","Урок 2","Урок 3","Урок 4","Урок 5","Урок 6","Урок 7","Урок 8","Урок 9","Урок 10","Урок 11","Урок 12","Урок 13"]
+
+les1Link = l.link1
+les1Name = "Введение в GIT"
+
+les2Link = l.link2
+les2Name = "Установка Git на Linux"
+
+les3Link = l.link3
+les3Name = "Git. Команды"
+
+les4Link = l.link4
+les4Name = "Git. Репозитории"
+
+les5Link = l.link5
+les5Name = "Git. Коммиты"
+
+les6Link = l.link6
+les6Name = "Git. История"
+
+les7Link = l.link7
+les7Name = "Git. Ветки"
+
+les8Link = l.link8
+les8Name = "Git. Публикация"
+
+les9Link = l.link9
+les9Name = "Git. Слияния"
+
+les10Link = l.link10
+les10Name = "Git. Версионирование"
+
+les11Link = l.link11
+les11Name = "Git. Pull-requests"
+
+les12Link = l.link12
+les12Name = "Git. Сложные операции"
+
+les13Link = l.link13
+les13Name = "Git. Работа с Fork"
